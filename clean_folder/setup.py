bd11f47caae76bfc5699c100c56a1d2e2f937416
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-07-15 03:17:02 +0300

    litle fix

---
+++ clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean_folder_145_236',
+      version='0.1.0',
+      description='Very useful code',
+      url='https://github.com/Yevhenii3145/python-core-home_work_7',
+      author='Eugen',
+      author_email='zakolochennoe457@gmail.com',
+      license='Apache',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean_folder = clean_folder:start']},
+      )
